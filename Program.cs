5b99e027a95925340bc8330c47403ca813885748
st <108557612+valeurs@users.noreply.github.com>
2024-03-26 05:47:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using CS2HizOlcer;
+using Swed64;
+using System.Diagnostics;
+using System;
+using System.Numerics;
+
+System.Diagnostics.Process.Start(new ProcessStartInfo
+{
+    FileName = "https://discord.gg/taDUKGdxJK",
+    UseShellExecute = true
+});
+
+Swed swed = new Swed("cs2");
+
+IntPtr client = swed.GetModuleBase("client.dll");
+
+Renderer renderer = new Renderer();
+Thread renderThread = new Thread(new ThreadStart(renderer.Start().Wait));
+renderThread.Start();
+
+int dwLocalPlayerPawn = 0x17371A8; //https://github.com/a2x/cs2-dumper
+
+int m_vecAbsVelocity = 0x3D8; //https://github.com/a2x/cs2-dumper
+
+while (true)
+{
+    IntPtr localPlayerPawn = swed.ReadPointer(client, dwLocalPlayerPawn);
+
+    Vector3 velocity = swed.ReadVec(localPlayerPawn, m_vecAbsVelocity);
+
+    int speed = (int)Math.Sqrt(velocity.X * velocity.X + velocity.Y * velocity.Y + velocity.Z * velocity.Z);
+
+    renderer.speed = speed;
+}
---
+++ Renderer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using ClickableTransparentOverlay;
+using ImGuiNET;
+
+namespace CS2HizOlcer
+{
+    public class Renderer : Overlay
+    {
+        public int speed = 0;
+        int maxSpeed = 0;
+
+        Vector4 white = new Vector4(1, 1, 1, 1);
+        Vector4 green = new Vector4(0, 1, 0, 1);
+        Vector4 yellow = new Vector4(1, 1, 0, 1);
+        Vector4 red = new Vector4(1, 0, 0, 1);
+
+        protected override void Render()
+        {
+            if (speed > maxSpeed)
+                maxSpeed = speed;
+
+            ImGui.Begin("Hiz Olcer");
+
+            ImGui.TextColored(GetSpeedColor(maxSpeed), $"Max Hiz: {maxSpeed}");
+            ImGui.TextColored(GetSpeedColor(speed), $"Simdiki hiz: {speed}");
+            ImGui.TextColored(red, "Yazar: scarew");
+        }
+
+        Vector4 GetSpeedColor(int speed)
+        {
+            if (speed > 500)
+                return red;
+            if (speed > 400)
+                return yellow;
+            if (speed > 300)
+                return green;
+            return white;
+        }
+
+    }
+}
